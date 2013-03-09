41b71b175f51df3c6be0d4b693c7a78ed14669e0
Romain Gauthier <romain.gauthier@monkeypatch.me>
2013-03-09 22:12:02 +0100

    Fix Content-Type check

--- requests_eventsource/eventsource.py
+++ requests_eventsource/eventsource.py
@@ -5,7 +5,7 @@ class ContentTypeError(Exception):
     pass
 
 def eventsource(response):
-    if response.headers['Content-Type'] is not 'text/event-stream':
+    if response.headers['Content-Type'] != 'text/event-stream':
         message = 'Bad Content-Type, found "%s" instead of "text/event-stream"'
         raise ContentTypeError(message % response.headers['Content-Type'])
 
